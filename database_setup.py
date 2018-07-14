1d9ce6c8774c4e31476602d11d71220a307d621e
Fernando Gonzalez <fgonz009@ucr.edu>
2018-07-14 12:28:20 -0700

    Added  google plus login using OAuth, and created a local permission system for the restaurant app.

--- database_setup.py
+++ database_setup.py
@@ -5,16 +5,26 @@ from sqlalchemy import create_engine
  
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+    
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    email = Column(String(250), nullable = False)
+    picture = Column(String(250))
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
+        """Return object data in easily serializeable format"""
+        return {
            'name'         : self.name,
            'id'           : self.id,
        }
@@ -23,19 +33,20 @@ class MenuItem(Base):
     __tablename__ = 'menu_item'
 
 
-    name =Column(String(80), nullable = False)
+    name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     description = Column(String(250))
     price = Column(String(8))
     course = Column(String(250))
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
-
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)    
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
+        """Return object data in easily serializeable format"""
+        return {
            'name'         : self.name,
            'description'         : self.description,
            'id'         : self.id,
@@ -45,7 +56,7 @@ class MenuItem(Base):
 
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
  
 
 Base.metadata.create_all(engine)
